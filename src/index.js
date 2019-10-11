946b6197d4b4e2dff450fb19cfa8330f3701d81b
Filip Sheronau <Filsheronov@gmail.com>
2019-10-11 23:07:51 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
+module.exports = function getTimeForEducation(focus,knowsProgramming,config) {	
+	const base = 500;
+	const master = 800;
+	let level = knowsProgramming ? master : master+base;	
+	let	result = Math.ceil(level/(config[focus]));
+	return result;
+};
   
